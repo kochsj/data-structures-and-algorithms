221431251c569c8b1727bb18edb63b2938b576b0
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-11-14 09:34:41 -0800

    solved challenge 3

--- code-challenges/Includes-every-substring-charAt/challenges-13.test.js
+++ code-challenges/Includes-every-substring-charAt/challenges-13.test.js
@@ -41,7 +41,10 @@ For example, (123) 456-7890 returns 1234567890
 ------------------------------------------------------------------------------------------------ */
 
 const standardizePhoneNumbers = (arr) => {
-  // Solution code here...
+  let regex = /[^0-9]/gm;
+  return arr.map(phoneNum => {
+    return phoneNum.replace(regex, '');
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
